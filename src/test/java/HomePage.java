f09fd7ee291a7e2a889fe8c2d67e27b655ae691c
Влад Шиллер <vladshiller@MacBook-Air-Vlad.local>
2025-09-26 12:07:21 +0300

    Initial commit

---
+++ src/test/java/HomePage.java
@@ -0,0 +1,78 @@
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class HomePage {
+
+    static String adress = "https://www.demoblaze.com/";
+    static String goToLoginMenu = "//*[@id=\"login2\"]";
+    static String loginInput = "//*[@id=\"loginusername\"]";
+    static String loginPassInput = "//*[@id=\"loginpassword\"]";
+    static String loginButton = "//*[@id=\"logInModal\"]/div/div/div[3]/button[2]";
+    static String logoutMenu = "//*[@id=\"logout2\"]";
+    static String goToSingUpMenu = "//*[@id=\"signin2\"]";
+    static String loginSingUpInput = "//*[@id=\"sign-username\"]";
+    static String passSingUpInput = "//*[@id=\"sign-password\"]";
+    static String singUpButton = "//*[@id=\"signInModal\"]/div/div/div[3]/button[2]";
+
+    public static void openHomePage(){
+
+        open(adress);
+
+    }
+
+    public static void goToLogin(){
+
+        $(By.xpath(goToLoginMenu)).click();
+
+    }
+
+    public static void enterUserNameLogin(String name){
+
+        $(By.xpath(loginInput)).sendKeys(name);
+
+    }
+
+    public static void enterPassLogin(String pass){
+
+        $(By.xpath(loginPassInput)).sendKeys(pass);
+
+    }
+
+    public static void clickLogin(){
+
+        $(By.xpath(loginButton)).click();
+
+    }
+
+    public static void clickLogOut(){
+
+        $(By.xpath(logoutMenu)).click();
+
+    }
+
+    public static void goToSingUp(){
+
+        $(By.xpath(goToSingUpMenu)).click();
+
+    }
+
+    public static void enterUserNameSingUp(String name){
+
+        $(By.xpath(loginSingUpInput)).sendKeys(name);
+
+    }
+
+    public static void enterPassSingUp(String pass){
+
+        $(By.xpath(passSingUpInput)).sendKeys(pass);
+
+    }
+
+    public static void clickSingUp(){
+
+        $(By.xpath(singUpButton)).click();
+
+    }
+
+}
