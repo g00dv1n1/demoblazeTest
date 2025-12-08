4d8d5e131971bb77491b3358bd8c5c0c14aaae30
vladshiller <vladilslav.shiller@gmail.com>
2025-12-08 17:25:21 +0300

    Remote driver config 2

--- src/test/java/config/BaseTest.java
+++ src/test/java/config/BaseTest.java
@@ -5,22 +5,19 @@ import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public abstract class BaseTest {
-
     @BeforeAll
     static void setUp() {
-        // идём на Selenium-контейнер из docker-compose
-        Configuration.remote = "http://selenium:4444/wd/hub";
-
-        Configuration.browser = "chrome";        // для standalone-chromium это ок
-        Configuration.headless = true;          // в контейнере GUI не нужен
-        Configuration.timeout = 8000;
+        Configuration.browser = "chrome";
+        Configuration.headless = true; // или оставь, если уже задаёшь где-то
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments(
+                "--headless=new",
                 "--no-sandbox",
+                "--disable-setuid-sandbox",
                 "--disable-dev-shm-usage",
                 "--disable-gpu",
-                "--window-size=1920,1080"
+                "--window-size=1366,768"
         );
 
         Configuration.browserCapabilities = options;
